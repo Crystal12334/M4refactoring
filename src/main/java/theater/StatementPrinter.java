e870698eb9e3fee6d775231bf2982b86ca036c5e
Qishan Wei <crystalwei06@163.com>
2025-11-11 20:45:17 -0500

    updated Task 2.4

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -44,24 +44,23 @@ public class StatementPrinter {
         final StringBuilder result = new StringBuilder("Statement for "
                 + invoice.getCustomer() + System.lineSeparator());
 
-        // final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
         for (Performance performance : invoice.getPerformances()) {
-            final Play play = plays.get(performance.getPlayID());
-            volumeCredits += getTotalVolumeCredits(performance, play);
+            final Play play = getPlay(performance);
+            volumeCredits += getVolumeCredits(performance, play);
         }
+
         for (Performance performance : invoice.getPerformances()) {
             totalAmount += getTotalAmount(performance);
         }
 
         for (Performance performance : invoice.getPerformances()) {
-            final Play play = plays.get(performance.getPlayID());
+            final Play play = getPlay(performance);
             final int thisAmount = getTotalAmount(performance);
             result.append(String.format("  %s: %s (%s seats)%n", play.getName(),
-                    getFormattedAmount(thisAmount), performance.getAudience()));
+                    usd(thisAmount), performance.getAudience()));
         }
 
-        result.append(String.format("Amount owed is %s%n", getFormattedAmount(totalAmount)));
+        result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
         return result.toString();
     }
@@ -77,7 +76,7 @@ public class StatementPrinter {
                             * (performance.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
                 }
                 break;
-            case Constants.COMEDY_TYPE:
+            case "comedy":
                 thisAmount = Constants.COMEDY_BASE_AMOUNT;
                 if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
                     thisAmount += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
@@ -90,17 +89,6 @@ public class StatementPrinter {
                 throw new RuntimeException(String.format("unknown type: %s", play.getType()));
         }
         return thisAmount;
-    }
-
-    private int getTotalVolumeCredits(Performance performance, Play play) {
-        int result = 0;
-        // add volume credits
-        result += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
-        // add extra credit for every five comedy attendees
-        if (Constants.COMEDY_TYPE.equals(play.getType())) {
-            result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
-        }
-        return result;
     }
 
     private int getTotalAmount(Performance performance) {
@@ -133,4 +121,35 @@ public class StatementPrinter {
         final NumberFormat usd = NumberFormat.getCurrencyInstance(Locale.US);
         return usd.format(amount / Constants.PERCENT_FACTOR);
     }
+
+    /**
+     * Returns the Play associated with the given performance.
+     *
+     * @param performance the performance object for which to get the associated play.
+     * @return the Play object associated with the performance.
+     */
+    public Play getPlay(Performance performance) {
+        return plays.get(performance.getPlayID());
+    }
+
+    private int getVolumeCredits(Performance performance, Play play) {
+        int result = 0;
+        result += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+        if (Constants.COMEDY_TYPE.equals(play.getType())) {
+            result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        }
+        return result;
+    }
+
+    /**
+     * Converts the given amount into a USD formatted string.
+     *
+     * @param amount the amount in cents to be formatted into USD.
+     * @return the formatted string in USD currency format.
+     */
+    public String usd(int amount) {
+        final NumberFormat usd = NumberFormat.getCurrencyInstance(Locale.US);
+        return usd.format(amount / Constants.PERCENT_FACTOR);
+    }
+
 }
